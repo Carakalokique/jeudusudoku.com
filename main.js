9726a1ef8f53cd0f815d826a8978d879a79403fd
Kenny Alami <kennyalami@Kennys-MacBook-Pro.local>
2024-08-05 12:46:42 -0700

    made the footer in a component

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+// Fetch footer
+
+// Function to include HTML content
+async function includeHTML() {
+  const elements = document.querySelectorAll('[data-include]');
+  for (const element of elements) {
+      const file = element.getAttribute('data-include');
+      try {
+          const response = await fetch(file);
+          if (response.ok) {
+              const text = await response.text();
+              element.innerHTML = text;
+          } else {
+              console.error('Error fetching file:', file, response.status);
+          }
+      } catch (error) {
+          console.error('Error fetching file:', file, error);
+      }
+  }
+}
+
+// Call the function once the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', includeHTML);
+
+
+
 import { Cell, Group } from "./Cell.js";
 
 let difficulty =
